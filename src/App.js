a64dbc812203bf02e8ab611df4036513f499bcfc
Alejandrovc6467 <alejandrovc6467@gmail.com>
2023-05-25 06:56:40 -0600

    solo el Validation en los textfield

--- src/App.js
+++ src/App.js
@@ -7,21 +7,26 @@ import './App.css';
 import ClassComponent from './components/ClassComponent';
 import FuncComponent from './components/FuncComponent';
 import FormSignUp from './components/FormSignUp';
+import  Container  from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 
 //➡️ este proyecto busca explicar y dar un ejemplo de las diferencias de un componente de clase y un componente funcional, los ejemplos estan en los archivos ClassComponent y FuncComponent, si los quiero ver en en accion los llamo en el App.js y listo, solo para ver ejemplo corriendo
-//ademas se utiliza Material UI en este proyecto , este es el comando para instalar esa libreria  npm install @mui/material @emotion/react @emotion/styled   en esta misma url estan los componentes de donde voy a sacar el codigo  https://mui.com/material-ui/getting-started/installation/
+//ademas se utiliza ➡️Material UI➡️ en este proyecto , este es el comando para instalar esa libreria  npm install @mui/material @emotion/react @emotion/styled   en esta misma url estan los componentes de donde voy a sacar el codigo  https://mui.com/material-ui/getting-started/installation/
 
 
 
+//utilizo un Container para envolver todo el form y el h2, esto es un componente de mui, se encuentra en la pagina oficial en components en el aprtado de layouts, lo presionas y vas a la API, Ahi estan todos los atributos que recibe y asi voy a ssaber que me serviria usar en cada caso
+// ahora si yo quisier poner mi formulario a la izquierda y otra cosa a la derecha o hacer mas cosas especificas pues creo un css y si utilizo felxbox margenes o lo que yo quiera, tengo que tener en cuenta que esto me da un componente ya estilizado pero el lugar de acodo lo pongo de todos estsos elementos los pongo yo con un flex box o lo que yo quiera
 
 function App() {
   return (
-    <div>
+    <Container  component="section" maxWidth="sm">{/*este Container de mui recibe un tipo de componente para igualarlo  y tambien maxwidth entre otros */}
      
+      <Typography variant='h3'  align='center' component="h1" >Formulario Registro</Typography>{/** Para los h1 y demas se utiliza el componente typography de mui, para usarlo hay que importarlo aqui en este codigo con el import y ademas hay poner l link del cdn en el index del proyecto */}
       <FormSignUp/>
 
-    </div>
+    </Container>
   );
 }
 
--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,32 +1,55 @@
 
 // este import es necesario para los componentes que yo uso y lo tomo desde la pagina de mui.com
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+
+
+import { useState } from 'react';
+
 // y los componentes se utilizan con sus diferentes variables que las recibe por el atributo variant, tambien se les puede cambiar el color y tamanio y demas cosas, solo es de fijarse en la pagina cuales son los atributos que recibe
 // estos  variant="text">  variant="contained">  variant="outlined">
 //los atributos entre los componentes pueden varian este es solo un ejemlplo de los que llevan los buttom
 
-
+// en la pagina en el componente que me encuentre abajo hay un apartado que dice API y tiene una lista en esa lista al final esta el nomnbre del y link del componente si presion ahi me redirije a todos sus atributos de ese componente es importante eso para saber que es lo que quiero aplicarle que estilo por ejemplo el atributo margin y todos esos 
 
 function FormSignUp() {
 
+    //variables para controlar los estados de los input
+    // esta forma de manipular los daros se llama controlled components/ componentes controlado, es mas que todo para controlar los datos ingresados en formulario como para hacer validaciones de datos antes de enviarlos
+    const [name, setName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [promociones, setPromociones] = useState(true)
+    const [novedades, setNovedades] = useState(true)
+
+
+    const manejarEnvio = (e) => {
+        //el e.preventDefault es el encargado de NO recargar la pagina y solo llamar a esta funcion y realizar lo que este escrito aquí, claro el formulario llama a la funcion onSubmit y le pasa por parametro esta funcion
+        e.preventDefault()
+        console.log({name, lastName, email, promociones, novedades})
+    } 
+
+    
     return ( 
 
-        <form>
-            <label>Nombre</label>
-            <input type="text"/>
+        <form onSubmit={manejarEnvio}>
+          
+            <TextField id='name' label="Nombre" variant='outlined'  fullWidth margin='normal' value={name} onChange={(e) => { setName(e.target.value) }}/>
 
-            <label>Apellidos</label>
-            <input type="text"/>
+            <TextField id='lastName' label="Apellidos" variant='outlined' fullWidth margin='normal'  value={lastName} onChange={(e) => { setLastName(e.target.value) }}/>
 
-            <label>Email</label>
-            <input type="email"/>
+            <TextField id='email' label="Email" variant='outlined' fullWidth margin='normal' value={email} onChange={(e) => { setEmail(e.target.value) }} />
+
+            <FormGroup>
+                <FormControlLabel control={<Switch defaultChecked />} label="Promociones"  value={promociones} onChange={(e) => { setPromociones(!promociones) }} />
+                <FormControlLabel control={<Switch defaultChecked />} label="Novedades" value={novedades} onChange={(e) => { setNovedades(!novedades) }} />
+            </FormGroup>
             
-            <label>Promociones</label>
-            <input type="checkbox"/>
+            <Button variant="contained" type='submit' >Registrar</Button>
 
-            <label>Novedades</label>
-            <input type="checkbox"/>
-            <Button variant="contained">Registrar</Button>
         </form>
     
     )
